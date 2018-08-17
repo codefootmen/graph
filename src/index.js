d4f1c390d0e9bf380ecd479746571766608a9c14
Flavio Barci <flavio.barci@gmail.com>
2018-08-16 21:16:45 -0300

    Add  ES6 support with bundler boilerplate

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import _ from 'lodash';
+
+console.log();
+
+let root = document.getElementById('root');
+
+root.appendChild(document.createTextNode(_.capitalize('hello World')));
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["babel-preset-env"]
+          }
+        }
+      }
+    ]
+  }
+};
