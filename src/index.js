b8654d3ef20a49c1c98f6936b70232f7c518685d
Flavio Barci <flavio.barci@gmail.com>
2018-09-01 01:17:22 -0300

    Add global graph

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import JsonGraph from './app/jsonGraph';
 import WindowEvents from './view/windowEvents';
 import ButtonEvents from './view/buttonEvents';
 
+let graph = new JsonGraph();
+window.graph = graph;
 
 WindowEvents();
 ButtonEvents();
