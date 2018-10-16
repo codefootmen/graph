f6cae5485beb7ce864182038f129b0f0957bb6c8
Flavio Barci <flavio.barci@gmail.com>
2018-10-16 19:43:07 -0300

    Change backend to express

--- server/app.js
+++ server/app.js
@@ -1,8 +1,24 @@
-require('dotenv').config();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const port = 5000;
 
-const server = require('./middleware');
-const port = process.env.PORT || 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(fileUpload());
 
-server.listen(port, () => {
-  console.info('Server is running on port ' + port);
-})
+app.post('/upload', (req, res, next) => {
+    let imageFile = req.files.file;
+    imageFile.mv(__dirname + '/xml/graph.xml', function (err) {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.status(200).send(`File ${imageFile.name} uploaded`);
+        console.log(`File ${imageFile.name} uploaded`);
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
