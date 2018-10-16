e4e859c55135ba874a2a43d7a142129d5b2fd3bf
Felipe Toshio <felipetoshio1214@gmail.com>
2018-10-16 10:56:41 -0300

    Add xml2js and js2xml functions

--- server/http/routes.js
+++ server/http/routes.js
@@ -1,9 +1,39 @@
+const xml2js = require("xml2js");
+const fs = require('fs');
+const json2xml = require("json2xml");
+
 const routes = (server) => {
 
     server.get('/', (req, res, next) => {
         res.send(200, "Ok");
         next();
     });
+
+    server.get('/read1', (req, res, next) => {
+        var parser = new xml2js.Parser();
+        fs.readFile(__dirname + '/../xml/graph.xml', function (err, data) {
+            parser.parseString(data, function (err, result) {
+                res.send(200, JSON.stringify(result));
+                next();
+            });
+        });
+    });
+
+    server.get('/read2', (req, res, next) => {
+        fs.readFile(__dirname + '/../json/graph.json', function (err, data) {
+            fs.writeFile('convert.xml', json2xml(JSON.parse(data)), function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+
+                console.log("The file was saved!");
+            });
+            res.send(200, "Ok");
+            next();
+        });
+    });
+
+
 
 }
 
