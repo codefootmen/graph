dd9693f8ba49be279f3b94a73f050517796167af
Felipe Toshio <felipetoshio1214@gmail.com>
2018-10-16 15:13:40 -0300

    WIP

--- server/http/routes.js
+++ server/http/routes.js
@@ -9,26 +9,22 @@ const routes = (server) => {
         next();
     });
 
-    server.get('/read1', (req, res, next) => {
-        var parser = new xml2js.Parser();
-        fs.readFile(__dirname + '/../xml/graph.xml', function (err, data) {
-            parser.parseString(data, function (err, result) {
-                res.send(200, JSON.stringify(result));
-                next();
-            });
-        });
-    });
+    // server.post('/read', (req, res, next) => {
+    //     fs.readFile(__dirname + '/../xml/graph.xml', function (err, data) {
+    //         parser.parseString(data, function (err, result) {
+    //             res.send(200, JSON.stringify(result));
+    //             next();
+    //         });
+    //     });
+    // });
 
-    server.get('/read2', (req, res, next) => {
-        fs.readFile(__dirname + '/../json/graph.json', function (err, data) {
-            fs.writeFile('convert.xml', json2xml(JSON.parse(data)), function (err) {
-                if (err) {
-                    return console.log(err);
-                }
-
-                console.log("The file was saved!");
-            });
-            res.send(200, "Ok");
+    server.post('/read', (req, res, next) => {
+        fs.writeFile('graph.xml', JSON.stringify(req.body), function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+            res.send(200, req._body);
             next();
         });
     });
