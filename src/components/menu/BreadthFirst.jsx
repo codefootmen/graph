118739968ad2fc7b2222d0db5dcb34043cc50ba5
Felipe Toshio <felipetoshio1214@gmail.com>
2018-12-05 02:15:31 -0200

    Add BreadthFirst

---
+++ src/components/menu/BreadthFirst.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Button, Control, Field, Label } from 'bloomer';
+import Select from './withSelect';
+
+class BreadthFirst extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            start: ''
+        }
+
+        this.handleStart = this.handleStart.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleStart(event) {
+        this.setState({
+            start: event.target.value,
+        });
+    }
+
+    handleSubmit() {
+        this.props.handler({
+            breadthFirst: this.props.graph.breadthFirst({start: this.state.start})
+        });
+    }
+
+    render() {
+        return (
+            <div className="menu-row">
+                <Field>
+                    <Label>Breadth First
+                    </Label>
+                    <Control className="edge-selects-wrapper">
+                        <Select onChange={this.handleStart} options={this.props.graph} />
+                    </Control>
+                </Field>
+                <Button isColor='info' onClick={this.handleSubmit}>Find</Button>
+            </div>
+        );
+    }
+}
+
+export default BreadthFirst;
